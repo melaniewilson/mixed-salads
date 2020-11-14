21a631fc9d40810d38b143a05ebec7b5a57a0606
Melanie Wilson <melaniew1357@gmail.com>
2020-11-14 11:09:36 -0800

    file add

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat templates/top.html content/index.html templates/bottom.html > index.html
+cat templates/top.html content/about.html templates/bottom.html > about.html
+cat templates/top.html content/blog.html templates/bottom.html > blog.html
+cat templates/top.html content/projects.html templates/bottom.html > projects.html
