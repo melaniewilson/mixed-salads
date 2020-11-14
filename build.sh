bde89f911c2b4f3ec199d2ef0dd02ef4fbeb69a6
Melanie Wilson <melaniew1357@gmail.com>
2020-11-14 11:16:14 -0800

    clean up of error

--- build.sh
+++
@@ -1,4 +0,0 @@
-cat templates/top.html content/index.html templates/bottom.html > index.html
-cat templates/top.html content/about.html templates/bottom.html > about.html
-cat templates/top.html content/blog.html templates/bottom.html > blog.html
-cat templates/top.html content/projects.html templates/bottom.html > projects.html
